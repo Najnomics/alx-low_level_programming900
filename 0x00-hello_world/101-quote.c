ce4046113df311a350c40eb0c7cc6ecf55b05a0c
nosakhare <jesuorobonosaknosakhare873@gmail.com>
2022-06-30 19:59:30 -0700

    k

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *main - Prints a quote
- *Return: Always 0 (Success)
+ *main - Entry point
+ *Description:and that piece of art is useful
+ *Return: Always 0 (success)
  */
 int main(void)
 {
-		puts(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }

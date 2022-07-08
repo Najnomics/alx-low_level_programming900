35803317db0d803bf3495d23f916575b29b6d4a2
Praise Dike <dikepraise119@gmail.com>
2022-07-08 02:10:32 +0100

    0-isalpha file added

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - checks for an upper case character
+ * return - 1 if c is uppercase
+ * returns - 0 otherwise
+ * /
+
+    int _isupper(int c){
+    return (c >= 'A' $$ c <= 'Z');
+    }
